8b7539827e7f857693affd0d5cb2d9ae772657cb
anirbanivs <anirban@iviewsense.com>
2023-12-19 16:56:19 +0530

    mister table migration

--- migrations/20231123100539-create-master-list-equipment.js
+++ migrations/20231123100539-create-master-list-equipment.js
@@ -10,62 +10,112 @@ module.exports = {
         type: Sequelize.INTEGER
       },
 
-      asset_identification_no: {
-        type: Sequelize.STRING,
+      standard_maintained: {
+        type: Sequelize.STRING(100),
         allowNull: false,
       },
-      equipment_name: {
-        type: Sequelize.STRING,
+      name_of_equipment: {
+        type: Sequelize.STRING(500),
         allowNull: false,
       },
-      equipment_make_or_nodel: {
-        type: Sequelize.STRING,
+      uid: {
+        type: Sequelize.STRING(100),
         allowNull: false,
       },
-      equipment_serial_no: {
-        type: Sequelize.STRING,
+      type_of_facility: {
+        type: Sequelize.STRING(200),
         allowNull: false,
       },
-      range_size: {
-        type: Sequelize.STRING,
+      make: {
+        type: Sequelize.STRING(200),
         allowNull: false,
       },
-      least_count: {
-        type: Sequelize.STRING,
+      model_type: {
+        type: Sequelize.STRING(200),
         allowNull: false,
       },
-      accuracy_or_acceptance: {
-        type: Sequelize.STRING,
+      year_Of_make: {
+        type: Sequelize.STRING(10),
         allowNull: false,
       },
-      year_of_purchase: {
-        type: Sequelize.STRING,
+      serial_no: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+      asset_number: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+      receipt_date: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      date_placed_in_service: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      range: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+      least_Count: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+      least_product_tolerance: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+      accuracy: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+      history_card_number: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+      department: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+      date_of_last_calibration_date: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      calibration_certificate_no: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+      calibration_frequency: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+      calibration_valid_upto: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      calibration_agency: {
+        type: Sequelize.STRING(300),
         allowNull: false,
       },
       calibrated_by: {
-        type: Sequelize.STRING,
+        type: Sequelize.STRING(100),
         allowNull: false,
       },
-
-      date_of_calibration: {
-        type: Sequelize.DATE,
+      equipment_status: {
+        type: Sequelize.STRING(100),
         allowNull: false,
       },
-      calibration_due_date: {
-        type: Sequelize.DATE,
+      traceability: {
+        type: Sequelize.STRING(100),
         allowNull: false,
       },
-
-      calibration_certificate_no: {
-        type: Sequelize.STRING,
+      next_calibration_reminder: {
+        type: Sequelize.STRING(100),
         allowNull: false,
       },
-      uncertainty: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      frequency_of_calibration: {
-        type: Sequelize.STRING,
+      remark: {
+        type: Sequelize.STRING(1000),
         allowNull: false,
       },
 
