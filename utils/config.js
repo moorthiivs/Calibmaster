f222c4958b2723ea65d0c5385d9806ccb35e24da
anirbanivs <anirban@iviewsense.com>
2024-04-03 10:39:20 +0530

    ulr no generateULRNumber method fixed

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   ADMIN_PASSWORD: "Good2023$#1",
   TOKEN_SECRET: "CalibMaster_Secret",
-  CUSTOMER_PORTAL_SERVER: "http://localhost:5001",
+  CUSTOMER_PORTAL_SERVER: "http://192.168.1.111:5001",
   CUSTOMER_PORTAL_PORT: 5001,
   NODE_ENV: 'development'
 };
