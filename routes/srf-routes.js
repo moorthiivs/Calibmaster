8ccb25972d1f5337c5d5405753aa9e5053d787a8
Karthick <karthik@iviewsense.com>
2024-09-30 15:59:52 +0530

    Delivery label changes

--- routes/srf-routes.js
+++ routes/srf-routes.js
@@ -21,6 +21,7 @@ router.post("/add", srfController.addSRFHandler);
 router.post("/getall", srfController.getSRFs);
 
 router.post("/getsrfbyid", srfController.getsrfbyId);
+router.post("/generatelabel", srfController.generateLableForDevice);
 
 router.post("/additemtosrf", srfController.addItemtoSRF);
 
