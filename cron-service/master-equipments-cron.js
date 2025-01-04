5841ba31b50b9ad4b96c84debef684a0f0c981c5
praveenivs <praveen@iviewsense.com>
2025-01-04 15:52:26 +0530

    cron job updates

--- cron-service/master-equipments-cron.js
+++ cron-service/master-equipments-cron.js
@@ -82,160 +82,158 @@ const sendMail = async (eachData) => {
 
 const emailRemainder_1 = async (req, res, next) => {
 
+    // try {
+    // cron.schedule('0 0 * * *', async function () {  // run every day at 12:00 AM
+
     try {
+        let masterLists = await MasterListEquipment.findAll({
+            include: ['lab']
+        });
 
-        cron.schedule('0 0 * * *', async function () {  // run every day at 12:00 AM
+        let responseArr = [];
+        let mail_count = 0;
 
-            try {
-                let masterLists = await MasterListEquipment.findAll({
-                    include: ['lab']
+        for (let eachRow of masterLists) {
+
+            const { email_smtp_server_host, email_smtp_server_port, sender_password, sender_email } = eachRow?.lab;
+
+            if (eachRow?.calibration_remainder_date_1 && email_smtp_server_host && email_smtp_server_port && sender_password && sender_email) {
+
+                // *** Reaminder Date in DD/MM/YYYY format ***
+                let reaminderDate = new Date(eachRow?.calibration_remainder_date_1).toLocaleDateString('en-GB'); // Formats as DD/MM/YYYY
+                // *** Today Date in DD/MM/YYYY format ***
+                const currentDate = new Date().toLocaleDateString("en-GB", {
+                    timeZone: "Asia/Kolkata"
                 });
 
-                let responseArr = [];
-                let mail_count = 0;
+                let status;
 
-                for (let eachRow of masterLists) {
+                if (currentDate === reaminderDate) {
+                    status = "Today send the mail to contact person";
 
-                    const { email_smtp_server_host, email_smtp_server_port, sender_password, sender_email } = eachRow?.lab;
+                    responseArr.push({
+                        serial_no: eachRow?.serial_no,
+                        status
+                    });
 
-                    if (eachRow?.calibration_remainder_date_1 && email_smtp_server_host && email_smtp_server_port && sender_password && sender_email) {
-
-                        // *** Reaminder Date in DD/MM/YYYY format ***
-                        let reaminderDate = new Date(eachRow?.calibration_remainder_date_1).toLocaleDateString('en-GB'); // Formats as DD/MM/YYYY
-                        // *** Today Date in DD/MM/YYYY format ***
-                        const currentDate = new Date().toLocaleDateString("en-GB", {
-                            timeZone: "Asia/Kolkata"
-                        });
-
-                        let status;
-
-                        if (currentDate === reaminderDate) {
-                            status = "Today send the mail to contact person";
-
-                            responseArr.push({
-                                serial_no: eachRow?.serial_no,
-                                status
-                            });
-
-                            const mailresponse = await sendMail(eachRow);
-                            mailresponse && mail_count++;
-                        } else {
-                            status = `The mail will send the contact person on ${reaminderDate}`
-                            responseArr.push({
-                                serial_no: eachRow?.serial_no,
-                                status
-                            });
-                        }
-                    }
+                    const mailresponse = await sendMail(eachRow);
+                    mailresponse && mail_count++;
+                } else {
+                    status = `The mail will send the contact person on ${reaminderDate}`
+                    responseArr.push({
+                        serial_no: eachRow?.serial_no,
+                        status
+                    });
                 }
-
-                const currentDateInIST = new Date().toLocaleString("en-CA", {
-                    timeZone: "Asia/Kolkata",
-                    weekday: "short",
-                    year: "numeric",
-                    month: "short",
-                    day: "2-digit",
-                    hour: "2-digit",
-                    minute: "2-digit",
-                    second: "2-digit",
-                    hour12: false
-                });
-                const currentDate = currentDateInIST.replace(/ 24:/, " 00:"); // In time change to  24:00:00 to 00:00:00
-                let data = `Cron Job attempt on Master Equipment calibration_remainder_date_1 at ${currentDate} Total sent Mail: ${mail_count} \n`;
-
-                fs.appendFile("cronLogger.txt", data, function (err) {
-                    if (err) throw err;
-                });
-            } catch (err) {
-                console.error('Error during cron job execution:', err);
             }
+        }
 
-        }, {
-            scheduled: true,
-            timezone: "Asia/Kolkata"  // Set the timezone to India Standard Time (IST)
-        })
+        const currentDateInIST = new Date().toLocaleString("en-CA", {
+            timeZone: "Asia/Kolkata",
+            weekday: "short",
+            year: "numeric",
+            month: "short",
+            day: "2-digit",
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit",
+            hour12: false
+        });
+        const currentDate = currentDateInIST.replace(/ 24:/, " 00:"); // In time change to  24:00:00 to 00:00:00
+        let data = `Cron Job attempt on Master Equipment calibration_remainder_date_1 at ${currentDate} Total sent Mail: ${mail_count} \n`;
+
+        fs.appendFile("cronLogger.txt", data, function (err) {
+            if (err) throw err;
+        });
     } catch (err) {
-        console.error('Error with cron job setup:', err);
+        console.error('Error during cron job execution:', err);
     }
+
+    //     }, {
+    //         scheduled: true,
+    //         timezone: "Asia/Kolkata"  // Set the timezone to India Standard Time (IST)
+    //     })
+    // } catch (err) {
+    //     console.error('Error with cron job setup:', err);
+    // }
 };
 
 const emailRemainder_2 = async (req, res, next) => {
 
+    // try {
+    // cron.schedule('0 0 * * *', async function () {
+
     try {
+        let masterLists = await MasterListEquipment.findAll({
+            include: ['lab']
+        });
 
-        cron.schedule('0 0 * * *', async function () {
+        let responseArr = [];
+        let mail_count = 0;
 
-            try {
-                let masterLists = await MasterListEquipment.findAll({
-                    include: ['lab']
+        for (let eachRow of masterLists) {
+
+            const { email_smtp_server_host, email_smtp_server_port, sender_password, sender_email } = eachRow?.lab;
+
+            if (eachRow?.calibration_remainder_date_2 && email_smtp_server_host && email_smtp_server_port && sender_password && sender_email) {
+
+                // *** Reaminder Date in DD/MM/YYYY format ***
+                let reaminderDate = new Date(eachRow?.calibration_remainder_date_2).toLocaleDateString('en-GB'); // Formats as DD/MM/YYYY
+                // *** Today Date in DD/MM/YYYY format ***
+                const currentDate = new Date().toLocaleDateString("en-GB", {
+                    timeZone: "Asia/Kolkata"
                 });
 
-                let responseArr = [];
-                let mail_count = 0;
+                let status;
 
-                for (let eachRow of masterLists) {
+                if (currentDate === reaminderDate) {
+                    status = "Today send the mail to contact person";
 
-                    const { email_smtp_server_host, email_smtp_server_port, sender_password, sender_email } = eachRow?.lab;
+                    responseArr.push({
+                        serial_no: eachRow?.serial_no,
+                        status
+                    });
 
-                    if (eachRow?.calibration_remainder_date_2 && email_smtp_server_host && email_smtp_server_port && sender_password && sender_email) {
-
-                        // *** Reaminder Date in DD/MM/YYYY format ***
-                        let reaminderDate = new Date(eachRow?.calibration_remainder_date_2).toLocaleDateString('en-GB'); // Formats as DD/MM/YYYY
-                        // *** Today Date in DD/MM/YYYY format ***
-                        const currentDate = new Date().toLocaleDateString("en-GB", {
-                            timeZone: "Asia/Kolkata"
-                        });
-
-                        let status;
-
-                        if (currentDate === reaminderDate) {
-                            status = "Today send the mail to contact person";
-
-                            responseArr.push({
-                                serial_no: eachRow?.serial_no,
-                                status
-                            });
-
-                            const mailresponse = await sendMail(eachRow);
-                            mailresponse && mail_count++;
-                        } else {
-                            status = `The mail will send the contact person on ${reaminderDate}`
-                            responseArr.push({
-                                serial_no: eachRow?.serial_no,
-                                status
-                            });
-                        }
-                    }
+                    const mailresponse = await sendMail(eachRow);
+                    mailresponse && mail_count++;
+                } else {
+                    status = `The mail will send the contact person on ${reaminderDate}`
+                    responseArr.push({
+                        serial_no: eachRow?.serial_no,
+                        status
+                    });
                 }
-
-                const currentDateInIST = new Date().toLocaleString("en-CA", {
-                    timeZone: "Asia/Kolkata",
-                    weekday: "short",
-                    year: "numeric",
-                    month: "short",
-                    day: "2-digit",
-                    hour: "2-digit",
-                    minute: "2-digit",
-                    second: "2-digit",
-                    hour12: false
-                });
-                const currentDate = currentDateInIST.replace(/ 24:/, " 00:"); // In time change to  24:00:00 to 00:00:00
-                let data = `Cron Job attempt on Master Equipment calibration_remainder_date_2 at ${currentDate} Total sent Mail: ${mail_count} \n`;
-
-                fs.appendFile("cronLogger.txt", data, function (err) {
-                    if (err) throw err;
-                });
-            } catch (err) {
-                console.error('Error during cron job execution:', err);
             }
+        }
 
-        }, {
-            scheduled: true,
-            timezone: "Asia/Kolkata"  // Set the timezone to India Standard Time (IST)
-        })
+        const currentDateInIST = new Date().toLocaleString("en-CA", {
+            timeZone: "Asia/Kolkata",
+            weekday: "short",
+            year: "numeric",
+            month: "short",
+            day: "2-digit",
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit",
+            hour12: false
+        });
+        const currentDate = currentDateInIST.replace(/ 24:/, " 00:"); // In time change to  24:00:00 to 00:00:00
+        let data = `Cron Job attempt on Master Equipment calibration_remainder_date_2 at ${currentDate} Total sent Mail: ${mail_count} \n`;
+
+        fs.appendFile("cronLogger.txt", data, function (err) {
+            if (err) throw err;
+        });
     } catch (err) {
-        console.error('Error with cron job setup:', err);
+        console.error('Error during cron job execution:', err);
     }
+
+    //     }, {
+    //         scheduled: true,
+    //         timezone: "Asia/Kolkata"  // Set the timezone to India Standard Time (IST)
+    //     })
+    // } catch (err) {
+    //     console.error('Error with cron job setup:', err);
+    // }
 };
 
 module.exports = {
