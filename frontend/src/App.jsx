d491305d8f38df06ff1c4def37fc9fa00dd02803
moorthiivs <moorthi@iviewsense.com>
2026-04-05 16:17:04 +0530

    Fix case-sensitive import path for AddULR component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -57,7 +57,7 @@ import QuotationConfig from "./components/BodyContent/Quotation/QuotationConfig/
 import QuotationConfigList from "./components/BodyContent/Quotation/QuotationConfig/QuotationConfigList";
 import QuotationItem from "./components/BodyContent/Quotation/QuotationItem/QuotationItem";
 import QuotationCustomerList from "./components/BodyContent/Quotation/QuotationItem/QuotationCustomerList";
-import AddUlr from "./components/BodyContent/AddULR/AddUlr";
+import AddUlr from "./components/BodyContent/AddULR/AddULR";
 import ListULR from "./components/BodyContent/AddULR/ListULR";
 import CreateUncertaintyParameter from "./components/BodyContent/Uncertainty-Parameter/CreateUncertaintyParameter";
 import ListUncertaintyParameter from "./components/BodyContent/Uncertainty-Parameter/ListUncertaintyParameter";
